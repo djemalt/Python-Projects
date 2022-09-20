632ce2c1dc0a90027667eb3c25e3e0f59a067a18
djemalt <96260003+djemalt@users.noreply.github.com>
2022-09-20 13:44:09 +0300

    Add files via upload

---
+++ quiz-game-start/question_model.py
@@ -0,0 +1,13 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+test = Question("hey","True")
+
+# test = Question()
+# test.text = "hey"
+# test.answer = "True"
+# print(test.text)
+# print(test.answer)
