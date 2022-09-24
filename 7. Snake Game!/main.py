58e7849b101b247273d4f597a55c6caff72993e8
djemalt <96260003+djemalt@users.noreply.github.com>
2022-09-24 12:41:56 +0300

    Add files via upload

---
+++ 7. Snake Game!/main.py
@@ -0,0 +1,59 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
+
+COLLISION = 295
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game!")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score_board = ScoreBoard()
+screen.listen()
+
+screen.onkeypress(snake.up, "Up")
+screen.onkeypress(snake.down, "Down")
+screen.onkeypress(snake.left, "Left")
+screen.onkeypress(snake.right, "Right")
+
+game_on = True
+
+while game_on:
+    screen.update()
+
+    # To make it Harder and Faster when you collect more, numbers can change but this is for example
+    if score_board.score < 5:
+        time.sleep(0.1)
+    elif score_board.score >= 5 and score_board.score < 10:
+        time.sleep(0.05)
+    elif score_board.score >= 10 and score_board.score < 15:
+        time.sleep(0.025)
+
+    elif score_board.score >= 15:
+        time.sleep(0.02)
+    snake.move()
+
+    # Detect Collision with Food!
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score_board.add_score()
+
+    # Detect Collision with Wall!
+    if snake.head.xcor() > COLLISION or snake.head.xcor() < -COLLISION or snake.head.ycor() > COLLISION or snake.head.ycor() < -COLLISION:
+
+        score_board.reset()
+        snake.reset()
+    # Detect Collision with Tail!
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            score_board.reset()
+            snake.reset()
+
+screen.exitonclick()
---
+++ 7. Snake Game!/scoreboard.py
@@ -0,0 +1,37 @@
+from turtle import  Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class ScoreBoard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+        self.penup()
+        self.goto(0, 260)
+        self.color("white")
+        self.score = 0
+        self.update_score()
+        self.hideturtle()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def add_score(self):
+        self.score += 1
+        self.update_score()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt","w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_score()
+
+
+
